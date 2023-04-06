05f07913639a8a29960ebb7d1843194d4731bbf9
unknown <progamerguy005@gmail.com>
2023-04-07 01:18:56 +1000

    some testing

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import random
 from PySide6.QtWidgets import QApplication
 from PySide6.QtQuick import QQuickView
 
+import ebooklib
+from ebooklib import epub
+from ebooklib.utils import debug
+
 if __name__ == "__main__":
+    
+    book = epub.read_epub('./test/test.epub')
+
+    debug(book.metadata)
+    
     app = QApplication()
     view = QQuickView()
 
