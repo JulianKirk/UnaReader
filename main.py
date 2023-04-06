0701888c8bb3d75817d438e1bd351f87e3d1ab37
unknown <progamerguy005@gmail.com>
2023-04-07 02:14:36 +1000

    lit we can access individual chapters now

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     
     book = epub.read_epub('./test/test.epub')
 
-    for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
-        print(item.get_content())
+    # for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+    #     print(item.get_content())
+
+    print(book.get_item_with_id("xhtml1429").get_content())
 
     # debug(book.get_metadata('DC', 'title'))
     # debug(book.get_metadata('DC', 'creator'))
